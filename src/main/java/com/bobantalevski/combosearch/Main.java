4953a4a08d9e88345b97efb8fbcef37576e176a4
edison696 <talevski.boban@gmail.com>
2017-11-25 23:13:55 +0100

    adding a Post-Redirect-Get pattern in returning search results

--- src/main/java/com/bobantalevski/combosearch/Main.java
+++ src/main/java/com/bobantalevski/combosearch/Main.java
@@ -3,17 +3,23 @@ package com.bobantalevski.combosearch;
 import com.bobantalevski.combosearch.helpers.MathHelper;
 import com.bobantalevski.combosearch.helpers.Permutation;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.*;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.redirect;
 
 public class Main {
+    private static final String RESULTS_MODEL_KEY = "model";
+
     public static void main(String[] args) {
+
         get("/", (req, res) -> {
-            return new ModelAndView(null, "index.hbs");
+            Map<String, Object> model = captureModelResults(req);
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/search", (req, res) -> {
@@ -35,10 +41,42 @@ public class Main {
                 Permutation.getCombination(searchTerms, searchTerms.length, i, searchCombos, generalSearchTerm);
             }
             model.put("searchCombos", searchCombos);
-
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+            setModelResults(req, model);
+            res.redirect("/");
+            return null;
+        });
     }
+
+    private static void setModelResults(Request req, Map<String, Object> model) {
+        req.session().attribute(RESULTS_MODEL_KEY, model);
+    }
+
+    private static Map<String, Object> getModelResults(Request req) {
+        // if there's no session, return null
+        if (req.session(false) == null) {
+            return null;
+        }
+        // if there is a session, but it doesn't contain our model with results, return null
+        if (!req.session().attributes().contains(RESULTS_MODEL_KEY)) {
+            return null;
+        }
+        // otherwise return our model containing results
+        return req.session().attribute(RESULTS_MODEL_KEY);
+    }
+
+    private static Map<String, Object> captureModelResults(Request req) {
+        // get the model results
+        Map<String, Object> modelResults = getModelResults(req);
+        // if there was anything returned, it means the model existed and
+        // we need to remove it from the session so it doesn't get included
+        // on subsequent requests
+        if (modelResults != null) {
+            req.session().removeAttribute(RESULTS_MODEL_KEY);
+        }
+        return modelResults;
+    }
+
+
 
 
 }
